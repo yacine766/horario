f359e4fda8687fbadbca3a859a8685543e7ee589
34603 <34603@p>
2021-02-28 17:38:17 +0100

    plantilla inicial microservicio horario

---
+++ src/test/java/com/hospital/horario/HorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
