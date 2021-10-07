46a2e6e5ff080ca4e161414dfd611032770519c0
34662 <34662@192.168.1.140>
2021-10-07 09:30:28 +0200

    feat_001: ResponseEntity

--- src/main/java/com/hospital/horario/controller/HorarioController.java
+++ src/main/java/com/hospital/horario/controller/HorarioController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hospital.horario.entity.Horario;
@@ -19,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-@RequestMapping("horarios")
 public class HorarioController {
 	@Autowired
 	public HorarioService horarioService;
@@ -41,8 +43,9 @@ public class HorarioController {
 	
 	//Insert Horario en base de datos
 	@RequestMapping (path="/horario",method = RequestMethod .POST)
-	public Horario  insertNewHorario(@RequestBody   Horario horario ) {
-		return horarioService .addNewHorario(horario);
+	public ResponseEntity<Horario>  insertNewHorario(@RequestBody Horario horario ) {
+		Horario hora = horarioService .addNewHorario(horario);
+		return new ResponseEntity<>(hora,HttpStatus.CREATED);
 	}
 	
 	//delete un horario de la BD
